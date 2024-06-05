5d28b4f7667acdbbc8e0c3fa9f0bd9c07e212a95
vatsal2351993 <127256560+vatsal2351993@users.noreply.github.com>
2024-06-05 17:20:54 +0000

    done

--- 01_basics/2_datatypes.js
+++ 01_basics/2_datatypes.js
@@ -11,3 +11,17 @@
 let name = "vatsal"
 let age = 18
 let isLoggin = false
+
+// premitive data type:
+// 7 datatypes: String, number, null, undefined, Boolean, symbol,BigInt
+
+
+// non-premitive/ ReferencedataTypes:
+// Array, object, functions
+
+
+const func = function(){
+    console.log('hello world');
+}
+
+func()
