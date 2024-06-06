750c92d0e873a4a4b0f3fd30356b3006b91fa26d
vatsal2351993 <127256560+vatsal2351993@users.noreply.github.com>
2024-06-06 20:42:44 +0000

    created

--- 03_basics/2_scops.js
+++ 03_basics/2_scops.js
@@ -1,7 +1,22 @@
 let a = 10
 const b = 20
 var c = 30
+// out side {} is a global scope
+// {} inside the braces is a block scope
 
 console.log(a);
 console.log(b);
 console.log(c);
+
+function one(){
+    const username = "vatsal"
+
+    function two(){
+        webiste = "Youtube"
+        // console.log(username);
+        console.log(webiste)
+    }
+    two()
+    console.log(username);
+}
+one()
