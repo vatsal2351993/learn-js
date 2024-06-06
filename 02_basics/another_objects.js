922a163ede89265af1a9e180c88e649a962153a8
vatsal2351993 <127256560+vatsal2351993@users.noreply.github.com>
2024-06-06 17:25:48 +0000

    done

--- 02_basics/another_objects.js
+++ 02_basics/another_objects.js
@@ -2,5 +2,31 @@
 const user = {}
 user.id = "123abc"
 user.name = "vatsal"
-user,isloggedIn = false
+user.isloggedIn = false
 console.log(user);
+
+const user2 = {
+    email : "mistry@google.com",
+    fullname : {
+        username : {
+            firstname : "vatsal",
+            lastname: "mistry"
+        }
+    }
+}
+
+console.log(user2.fullname.username);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+
+// const obj3 = Object.assign({},obj1, obj2)
+const obj3 = {...obj1, ...obj2}
+console.log(obj3)
+
+
+console.log(Object.keys(user));
+console.log(Object.values(user));
+console.log(Object.entries(user));
+
+console.log(user.hasOwnProperty('name'));
