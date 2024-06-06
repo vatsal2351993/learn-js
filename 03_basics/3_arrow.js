b0d2c3b8896c9728c014fab89de432cde3e57079
vatsal2351993 <127256560+vatsal2351993@users.noreply.github.com>
2024-06-06 21:05:01 +0000

    done

---
+++ 03_basics/3_arrow.js
@@ -0,0 +1,23 @@
+const user = {
+    username: "vatsal",
+    price: 999,
+    
+    welcomeMessage: function(){
+        console.log(`${this.username} ,welcome to website`)
+    }
+}
+
+
+// const addnum = (num1 ,num2) => {
+//     return num1 + num2
+// }
+
+// console.log(addnum(5, 4))
+
+// const addnum = (num1 ,num2) =>  num1 ** num2 / num1
+const addnum = (num1 ,num2) =>  ({username : "vatsal"})
+
+
+console.log(addnum(5, 4))
+
+
