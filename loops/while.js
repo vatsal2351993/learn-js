57b4bed99f39803bbe66ac9103b0c7bdb85125ad
vatsal2351993 <127256560+vatsal2351993@users.noreply.github.com>
2024-06-07 23:12:00 +0000

    done

---
+++ loops/while.js
@@ -0,0 +1,18 @@
+let i = 0
+
+while (i <= 10) {
+    console.log(`value of index is ${i}`);
+    i = i + 2
+}
+
+let myarr = ['flash', 'batman', 'ironman']
+let arr = 0
+while (arr < myarr.length) {
+    console.log(`value is ${myarr[arr]}`);
+    arr = arr + 1  
+}
+let score = 11
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score<10);
