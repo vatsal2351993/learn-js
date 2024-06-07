923203a51f1b5c50e42402914634c2b1e85cc1b4
vatsal2351993 <127256560+vatsal2351993@users.noreply.github.com>
2024-06-07 17:27:04 +0000

    don2

---
+++ controlflow/one.js
@@ -0,0 +1,12 @@
+// < , >, <= , >=, ==, !=, ===, !== ,
+
+const bal = 1000
+
+if (bal < 500) {
+    console.log("less than 500");
+} else if(bal < 750) {
+    console.log("less than 750");
+} else if (bal < 1200)  {
+    console.log("less than 1200");
+    console.log("you are done");
+}
---
+++ controlflow/switch.js
@@ -0,0 +1,32 @@
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("apr");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("aug");
+        break;
+
+    default:
+        log("matched")
+        break;
+}
